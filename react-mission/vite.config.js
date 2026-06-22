b5a17a0751b56880cc94cbc28f84f9d9e5cd349a
xoxo <sung021125@gmail.com>
2026-06-22 17:40:35 +0900

    chore: github pages base path 설정 변경

--- react-mission/vite.config.js
+++ react-mission/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 배포를 위한 베이스 경로 설정
+  base: '/React-Basic-Mission-1/',
 })
